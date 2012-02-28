4d4fefa8a82278e436db95883678d854e73232e7
Tom Brand <tom025@me.com>
2012-02-28 20:47:05 +0000

    Solution to a large problem.

--- lib/min_value_finder.rb
+++ lib/min_value_finder.rb
@@ -1,11 +1,27 @@
 class MinValueFinder
-  def initialize(function)
+  def initialize(function, max_num_of_threads=2040)
     @function = function
+    @max_num_of_threads = max_num_of_threads
   end
 
   def min_on(range)
-    range.inject([]) do |results, value|
-      results << Thread.new { @function.call(value) }
-    end.map(&:value).min
+    sub_solutions = []
+    range.each_slice(slice_size_for(range)) do |sub_p|
+      sub_solutions << solver(sub_p)
+    end
+    sub_solutions.map(&:value).min
+  end
+
+  private
+  def solver(problem)
+    Thread.new do
+      problem.inject([]) do |results, value|
+        results << @function.call(value)
+      end.min
+    end
+  end
+
+  def slice_size_for(problem)
+    (range.to_a.size.to_f / @max_num_of_threads.to_f).ceil
   end
 end
